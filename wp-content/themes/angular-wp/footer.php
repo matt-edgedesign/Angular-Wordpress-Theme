95e63aca6734eff7df3fef8d2c6053f8c49d7871
matt-edgedesign <matt@steve-edge.com>
2015-07-15 22:59:49 +0100

    Added Angular links and Route
    This was added to the theme index page, any scripts were added to the
    functions file

--- wp-content/themes/angular-wp/footer.php
+++ wp-content/themes/angular-wp/footer.php
@@ -1,8 +1,6 @@
-</div>
-<div id="footer"><?php bloginfo('name'); ?> is proudly using the <a href="http://ifelse.co.uk/simpla">Simpla theme</a> originally designed by <a href="http://ifelse.co.uk">Phu</a>. Powered by <a href="http://wordpress.org/">WordPress</a>
-<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
-<?php wp_footer(); ?>
-</div>
-
+<footer>
+		&copy; <?php echo date( 'Y' ); ?>
+	</footer>
+	 
 </body>
 </html>
