8d6589dd7d063db70608c2e57154118005b65e9b
matt-edgedesign <matt@steve-edge.com>
2015-07-15 23:20:17 +0100

    AngularJS Setup
    Partials and injector created of main.html

--- wp-content/themes/angular-wp/functions.php
+++ wp-content/themes/angular-wp/functions.php
@@ -1,13 +1,29 @@
-<?php function my_scripts() {
-	
-	wp_enqueue_script(
+<?php
+
+function my_scripts() {
+
+	wp_register_script(
 		'angularjs',
 		get_stylesheet_directory_uri() . '/bower_components/angular/angular.min.js'
 	);
 
-	wp_enqueue_script(
+	wp_register_script(
 		'angularjs-route',
 		get_stylesheet_directory_uri() . '/bower_components/angular-route/angular-route.min.js'
+	);
+
+	wp_enqueue_script(
+		'my-scripts',
+		get_stylesheet_directory_uri() . '/js/scripts.js',
+		array( 'angularjs', 'angularjs-route' )
+	);
+
+	wp_localize_script(
+		'my-scripts',
+		'myLocalized',
+		array(
+			'partials' => trailingslashit( get_template_directory_uri() ) . 'partials/'
+			)
 	);
 }
 
--- wp-content/themes/angular-wp/header.php
+++ wp-content/themes/angular-wp/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html ng-app="app">
 <head>
+	<base href="<?php $url_info = parse_url( site_url() ); echo trailingslashit( $url_info['path'] ); ?>">
 	<title>AngularJS Demo Theme</title>
 	<?php wp_head(); ?>
 </head>
