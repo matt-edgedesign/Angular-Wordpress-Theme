aa044f992c00ecb0dcd228e7b15eb7d2eda052f5
matt-edgedesign <matt@steve-edge.com>
2015-07-29 23:26:11 +0100

    Basic Bootstrap Styling Added

--- wp-content/themes/angular-wp/functions.php
+++ wp-content/themes/angular-wp/functions.php
@@ -27,4 +27,15 @@ function my_scripts() {
 	);
 }
 
-add_action( 'wp_enqueue_scripts', 'my_scripts' ); ?>
+add_action( 'wp_enqueue_scripts', 'my_scripts' ); 
+
+
+/* Add bootstrap support to the Wordpress theme*/
+
+function theme_add_bootstrap() {
+	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/css/bootstrap.min.css' );
+	wp_enqueue_style( 'style-css', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/css/css/js/bootstrap.min.js', array(), '3.0.0', true );
+}
+ 
+add_action( 'wp_enqueue_scripts', 'theme_add_bootstrap' ); ?>
--- wp-content/themes/angular-wp/header.php
+++ wp-content/themes/angular-wp/header.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <html ng-app="app">
+
 <head>
 	<base href="<?php $url_info = parse_url( site_url() ); echo trailingslashit( $url_info['path'] ); ?>">
 	<title>AngularJS Demo Theme</title>
